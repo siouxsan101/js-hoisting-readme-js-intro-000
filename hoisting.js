9f09313d27c0e23a507f0bb72e7d386a81b09ba2
susan hofmann <susanhofmannnyc@icloud.com>
2020-06-05 22:04:37 +0000

    Done.

--- hoisting.js
+++ hoisting.js
@@ -1,21 +1,19 @@
 function callMe() {
+  var lyric = "maybe";
   console.log("I just met you...");
   console.log("and this is crazy..");
   console.log("but here's my number..");
   console.log("so call me");
   return lyric;
-  var lyric = "maybe";
 }
-
 
 function crazy() {
-  // fix the code in here:
-  thisIsCrazy();
+ // fix the code in here:
 
-  var thisIsCrazy = function (){
-    console.log("hey!!!")
-  }
-}
+ var thisIsCrazy = function crazy(){
+   console.log("hey!!!");}
+   thisIsCrazy();
+ }
 
 function sayMyName() {
   // fix the code in here:
@@ -24,7 +22,8 @@ function sayMyName() {
   sayMy();
 
   function sayMy() {
-    console.log(name);
     var name = "Kristin";
+    console.log(name);
+
   }
 }
